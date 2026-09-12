c54d2f829b512b9161e0106c35b44e05b0bf1054
Badiboy <badialex@gmail.com>
2026-09-12 22:04:21 +0300

    Setup subsystem renewed

--- pyCryptomusAPI/version.py
+++ pyCryptomusAPI/version.py
@@ -1,3 +1,3 @@
 # Versions should comply with PEP440.
-# This line is parsed in setup.py:
-__version__ = '1.0.0'
+# This value is read by the build configuration in pyproject.toml:
+__version__ = '1.0.1'
